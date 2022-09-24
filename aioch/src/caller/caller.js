32dea4cd9717e340f4e08a4b5b956af5ff0eac0a
PimonovKostya <102628384+PimonovKostya@users.noreply.github.com>
2022-09-24 12:09:51 +0300

    йцу

--- aioch/src/caller/caller.js
+++ aioch/src/caller/caller.js
@@ -2,20 +2,20 @@ const {stdout} = require('process')
 const spawn = require('child_process').spawn
 const path = require('path');
 
-let text = '';
+let text = 'some new text';
 
 function setText(input){
     return text = {'text': input}
 }
 
 
-let stringifiedData = JSON.stringify(text);
+let stringifiedData = JSON.stringify({'text': text});
 
 let hellopath = path.resolve('../', 'py', 'helloworld.py');
 
 const py = spawn('python', [hellopath, stringifiedData]);
 
-resultString = '';
+let resultString = '';
 
 py.stdout.on('data', (stdData) =>{
     resultString += stdData.toString();
@@ -27,4 +27,4 @@ py.stdout.on('end', ()=>{
     console.log('You wrote :', text);
 })
 
-export default setText;
+// export default setText;
--- aioch/src/components/Input.jsx
+++ aioch/src/components/Input.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from "react";
 import caller from '../caller/caller.js';
 
 const Input = function (props) {
